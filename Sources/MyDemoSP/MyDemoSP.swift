adb3a44fc888a0a50f223a7667c93268103875ad
Alex Nagy <therebeloper@gmail.com>
2021-03-11 20:23:00 +0200

    first commit

--- Sources/MyDemoSP/MyDemoSP.swift
+++ Sources/MyDemoSP/MyDemoSP.swift
@@ -1,3 +1,19 @@
-struct MyDemoSP {
-    var text = "Hello, World!"
+//
+//  MyDemoSP.swift
+//  
+//
+//  Created by Alex Nagy on 11.03.2021.
+//
+
+import SwiftUI
+
+public extension View {
+    
+    func isInNavigationView() -> some View {
+        NavigationView { self }
+    }
+    
+    func isInStackNavigationView() -> some View {
+        NavigationView { self }.navigationViewStyle(StackNavigationViewStyle())
+    }
 }
